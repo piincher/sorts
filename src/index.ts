d5f142d35e8210e2927b51c6fef72e1f8d2d6bb8
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-23 06:56:52 +0800

    sorting scaffoling

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-console.log('hi there');
-console.log('typesce');
+class Sorter {
+	constructor(public collections: number[]) {}
+
+	sort(): void {}
+}
+const sorter = new Sorter([ 2, 34, 5, -1 ]);
+sorter.sort();
+
+console.log(sorter.collections);
