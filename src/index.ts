d1832e716a619be8d3b18fc38d9429175fc5aba6
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-24 16:09:02 +0800

    sorting implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 class Sorter {
 	constructor(public collections: number[]) {}
 
-	sort(): void {}
+	sort(): void {
+		const { length } = this.collections;
+
+		for (let i = 0; i < length; i++) {
+			for (let j = 0; j < length - i - 1; j++) {
+				if (this.collections[j] > this.collections[j + 1]) {
+					const leftHand = this.collections[j];
+					this.collections[j] = this.collections[j + 1];
+					this.collections[j + 1] = leftHand;
+				}
+			}
+		}
+	}
 }
-const sorter = new Sorter([ 2, 34, 5, -1 ]);
+const sorter = new Sorter([ 10, 3, -5, 0 ]);
 sorter.sort();
 
 console.log(sorter.collections);
