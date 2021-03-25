302d2306cdcbcf00fecd40924ace1aa579f7a1e4
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-26 07:13:17 +0800

    clean up inside the index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-class Sorter {
-	constructor(public collections: number[]) {}
-
-	sort(): void {
-		const { length } = this.collections;
-
-		for (let i = 0; i < length; i++) {
-			for (let j = 0; j < length - i - 1; j++) {
-				if (this.collections[j] > this.collections[j + 1]) {
-					const leftHand = this.collections[j];
-					this.collections[j] = this.collections[j + 1];
-					this.collections[j + 1] = leftHand;
-				}
-			}
-		}
-	}
-}
 const sorter = new Sorter([ 10, 3, -5, 0 ]);
 sorter.sort();
 
