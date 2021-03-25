29a5e89a9376cc5220a2997813c45309fb1eac4c
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-26 07:49:06 +0800

    refactorisation

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,15 +1,14 @@
+import { NumberCollections } from './NumberCollections';
 export class Sorter {
-	constructor(public collections) {}
+	constructor(public collections: NumberCollections) {}
 
 	sort(): void {
 		const { length } = this.collections;
 
 		for (let i = 0; i < length; i++) {
 			for (let j = 0; j < length - i - 1; j++) {
-				if (this.collections[j] > this.collections[j + 1]) {
-					const leftHand = this.collections[j];
-					this.collections[j] = this.collections[j + 1];
-					this.collections[j + 1] = leftHand;
+				if (this.collections.compare(j, j + 1)) {
+					this.collections.swap(j, j + 1);
 				}
 			}
 		}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-const sorter = new Sorter([ 10, 3, -5, 0 ]);
+import { Sorter } from './Sorter';
+import { NumberCollections } from './NumberCollections';
+const numberCollections = new NumberCollections([ 2, 1, -1, 3 ]);
+
+const sorter = new Sorter(numberCollections);
 sorter.sort();
 
-console.log(sorter.collections);
+console.log(numberCollections.data);
