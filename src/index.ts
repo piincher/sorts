276f4f49aa0509ac193bc09b2103d39d814e2318
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-27 07:16:13 +0800

    add character sorter inside the index for sorting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Sorter } from './Sorter';
 import { NumberCollections } from './NumberCollections';
-const numberCollections = new NumberCollections([ 2, 1, -1, 3 ]);
+import { CharacterCollection } from './CharacterCollection';
 
-const sorter = new Sorter(numberCollections);
+// const numberCollections = new NumberCollections([ 2, 1, -1, 3 ]);
+
+// const sorter = new Sorter(numberCollections);
+// sorter.sort();
+
+// console.log(numberCollections.data);
+
+const characterCollection = new CharacterCollection('Piincher');
+
+const sorter = new Sorter(characterCollection);
+
 sorter.sort();
-
-console.log(numberCollections.data);
+console.log(characterCollection.data);
