d4385eb0dfe95d371da59247e6c700685bda6010
Ibrahim <ikouma269@gmail.com>
2022-08-19 19:57:41 +0800

    sorting implementation

--- build/index.js
+++ build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("hi ther");
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sort = new Sorter([0, 1, -10, 4]);
+sort.sort();
+console.log("sort", sort.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  sort() {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
 }
+
+const sort = new Sorter([0, 1, -10, 4]);
+sort.sort();
+console.log("sort", sort.collection);
