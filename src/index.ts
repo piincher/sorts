3c14c49efb541ef54f5f5964d89f74fe97e2c127
Ibrahim <ikouma269@gmail.com>
2022-08-20 19:56:22 +0800

    final code of the project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumberCollection";
 import { CharacterClass } from "./CharacterCollection";
-// const numberCollection = new NumbersCollection([6, 3, -5, 0]);
-// const sort = new Sorter(numberCollection);
-// sort.sort();
+import { LinkedList } from "./LinkedList";
+const numberCollection = new NumbersCollection([6, 3, -5, 0]);
+numberCollection.sort();
 // console.log("hid");
-// console.log("sorct", numberCollection.data);
+console.log("sorct", numberCollection.data);
 
 const characterCollection = new CharacterClass("Xaayb");
+characterCollection.sort();
 
-const sorter = new Sorter(characterCollection);
-sorter.sort();
 console.log(characterCollection.data);
+const linkedList = new LinkedList();
+linkedList.sort();
+linkedList.add(500);
+linkedList.add(-3);
+linkedList.add(-1);
+
+linkedList.print();
